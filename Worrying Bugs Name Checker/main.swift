8881280ccea1964aeb4afb5c7266da274927c7db
Steven Berry <berrste@gmail.com>
2018-03-12 12:47:29 -0500

    Initial Commit

---
+++ Worrying Bugs Name Checker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Worrying Bugs Name Checker
+//
+//  Created by Steven Berry on 3/12/18.
+//  Copyright © 2018 Super Awesome Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
