f3969a92f653f3256a9984226dd41d9c72cf35c2
Steven Berry <berrste@gmail.com>
2018-03-12 15:55:13 -0500

    Version 1.0

--- Worrying Bugs Name Checker/main.swift
+++ Worrying Bugs Name Checker/main.swift
@@ -8,5 +8,114 @@
 
 import Foundation
 
-print("Hello, World!")
+extension String {
+    func indicesOf(string: String) -> [Int] {
+        var indices = [Int]()
+        var searchStartIndex = self.startIndex
+        
+        while searchStartIndex < self.endIndex, let range = self.range(of: string, range: searchStartIndex..<self.endIndex), !range.isEmpty {
+            let index = distance(from: self.startIndex, to: range.lowerBound)
+            indices.append(index)
+            searchStartIndex = range.upperBound
+        }
+        
+        return indices
+    }
+}
+
+var sema = DispatchSemaphore( value: 0 )
+var siteData = Data()
+
+class Delegate : NSObject, URLSessionDataDelegate
+{
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        siteData.append(data)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        sema.signal()
+    }
+}
+
+func internetOn() -> Bool {
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config, delegate: Delegate(), delegateQueue: nil )
+    
+    guard let url = URL( string:"http://isup.me" ) else { fatalError("Could not create URL object") }
+    
+    session.dataTask( with: url ).resume()
+    
+    sema.wait()
+    
+    if !siteData.isEmpty {
+        siteData = Data()
+        return true
+    } else {
+        return false
+    }
+}
+
+func getFeedData() -> Data? {
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config, delegate: Delegate(), delegateQueue: nil )
+    
+    guard let url = URL( string:"https://stevenberry.rocks/worryingbugs/feed/podcast/" ) else { fatalError("Could not create URL object") }
+    
+    session.dataTask( with: url ).resume()
+    
+    sema.wait()
+    
+    if !siteData.isEmpty {
+        return siteData
+    } else {
+        return nil
+    }
+}
+
+func checkTitle(string: String, feedData: Data) -> Bool {
+    if let stringData = String(data: feedData, encoding: .utf8) {
+        let startTitles = stringData.indicesOf(string: "<title>")
+        let endTitles = stringData.indicesOf(string: "</title>")
+        var titles = [String()]
+        
+        for i in 0..<startTitles.count {
+            let start = stringData.index(stringData.startIndex, offsetBy: startTitles[i] + 7)
+            let end = stringData.index(stringData.startIndex, offsetBy: endTitles[i] - 8)
+            let range = start..<end
+            titles.append(String(stringData[range]))
+        }
+        
+        for title in titles {
+            if title.lowercased().contains(string.lowercased()) {
+                return true
+            }
+        }
+        
+        return false
+    } else {
+        return true
+    }
+}
+
+
+if !internetOn() {
+    print("Please check your network connection and try again")
+    exit(0)
+}
+
+if let feedData = getFeedData() {
+    var anyUsed = false
+    for argument in CommandLine.arguments {
+        let used = checkTitle(string: argument, feedData: feedData)
+        if used {
+            anyUsed = true
+            print("\(argument) has already been used")
+        }
+    }
+    if !anyUsed {
+        print("All clear")
+    }
+}
+
+
 
